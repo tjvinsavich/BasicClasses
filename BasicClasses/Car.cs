06ffe4e35d5fbae5d0010ab3ee7c8bfbb00d1532
TJ Vinsavich <tjvinsavich@gmail.com>
2021-06-18 11:39:28 -0500

    initial commit

---
+++ BasicClasses/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicClasses
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ BasicClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BasicClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Honda";
+            myCar.Model = "CR-V";
+            myCar.Year = 2012;
+
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}");
+        }
+    }
+}
